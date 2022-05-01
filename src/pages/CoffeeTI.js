249043e839837563e7299b70a5c2ae20edea4e9c
jodoll147 <59550913+jodoll147@users.noreply.github.com>
2022-05-01 21:49:27 +0900

    coffeeTI 컨테이너

--- src/pages/CoffeeTI.js
+++ src/pages/CoffeeTI.js
@@ -1,5 +1,30 @@
+import react, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import CoffeTIContainer from './styled/CoffeeTIContainer';
+import CoffeeTI_first from './CoffeeTIPage/CoffeeTI_first';
+import CoffeeTI_select from './CoffeeTIPage/CoffeeTI_select';
+const coffeetiPage = () => {};
+
 const CoffeeTI = () => {
-  return <div>CoffeeTI</div>;
+  return (
+    <CoffeTIContainer>
+      <section align='center'>
+        <div className='back'>
+          <div class='container'>
+            <div className='logo'>
+              <Link to='/' style={{ textDecoration: 'none', color: '#F2F2F2' }}>
+                당원커여
+              </Link>
+            </div>
+            <div className='coffeeti'>
+              <p>CoffeeTI</p>
+            </div>
+            <div className='box'></div>
+          </div>
+        </div>
+      </section>
+    </CoffeTIContainer>
+  );
 };
 
 export default CoffeeTI;
---
+++ src/pages/styled/CoffeeTIContainer.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+const CoffeeTIContainer = styled.div`
+  section {
+    position: relative;
+    width: 940px;
+    height: 100vh: 
+    font-family: 'NanumGothic';
+    font-style: normal;
+    letter-spacing: -0.333333px;
+    margin: 0 auto;
+
+    .back {
+      position: relative;
+      width: 100%;
+      height: 100vh;
+      text-align: center;
+      background: linear-gradient(180deg, rgba(166, 140, 118, 0.61) 0%, #ccd9d5 63.26%);
+    }
+
+  }
+
+  .box{
+    position: relative;
+    left: 145px;
+    top: 10px;
+    width: 650px;
+    height: 600px;
+    background: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    text-align: center;
+  }
+
+  .logo {
+    position: relative;
+    top: 90px;
+    left: 145px;
+    font-weight: 800;
+    font-size: 30px;
+    text-align: left;
+    color: #f2f2f2;
+  }
+
+  .coffeeti{
+    p{
+      position: relative;
+      top: 45px;    
+      font-weight: 800;
+      font-size: 64px;
+      color: #594031;
+      text-align: center;
+    }
+  }
+
+
+  
+
+
+
+
+
+
+  }
+`;
+
+export default CoffeeTIContainer;
