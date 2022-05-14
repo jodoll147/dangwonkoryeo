40be488d17ff88a95a0b7be5190945bd0f408ce0
jodoll147 <59550913+jodoll147@users.noreply.github.com>
2022-05-14 22:40:20 +0900

    공지사항 style 수정

--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -1,22 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import NoticeContainer from './styled/NoticeContainer';
 
-const Notice = () => {
-  const notices = ['', '', '', '', '', '', ''];
-  const noticeList = notices.map(notice => <div className='noticeList'>{notice}</div>);
+const NoticeBox = styled.div`
+  position: relatvie;
+  display: flex;
+  width: 940px;
+  height: 50px;
+  .title {
+    position: relative;
+    padding-left: 10px;
+    width: 650px;
+    height: 50px;
+    text-align: left;
+    border: 1px solid #f2f2f2;
+    font-family: 'Cafe24';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 50px;
+  }
+  .date {
+    position: relative;
+    padding-left: 10px;
+    width: 290px;
+    height: 50px;
+    text-align: left;
+    border: 1px solid #f2f2f2;
+    font-family: 'Cafe24';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 50px;
+  }
+`;
 
+const WriteList = () => {
+  const [notice, setNotice] = useState([
+    { title: '이거시거늘', date: '22/05/14' },
+    { title: '촉촉슬림화장솜', date: '22/05/14' },
+    { title: '이 가게 저 가게 다니면서 라면을 보았네', date: '22/05/14' },
+    { title: '와 라면', date: '22/05/14' },
+  ]);
+
+  const noticeList = notice.map(temp => (
+    <NoticeBox>
+      <div className='title'>{temp.title}</div>
+      <div className='date'>{temp.date}</div>
+    </NoticeBox>
+  ));
+
+  return noticeList;
+};
+
+const Notice = () => {
   return (
     <NoticeContainer>
       <section>
         <p>공지사항</p>
-        <div className='write_th'>
-          <p className='title'>제목</p>
-          <p className='date'>날짜</p>
+        <div className='writeTh'>
+          <div className='hr'>
+            <div className='title'>제목</div>
+            <div className='date'>날짜</div>
+          </div>
+
+          <WriteList />
         </div>
-        {noticeList}
         <div className='writing'>
-          <Link to='/notice/notice_write' style={{ textDecoration: 'none', color: '#F2F2F2' }}>
+          <Link
+            to='/notice/notice_write'
+            className='writeBtn'
+            style={{ textDecoration: 'none', color: '#F2F2F2' }}
+          >
             글쓰기
           </Link>
         </div>
---
+++ src/pages/styled/ShopModifyContainer.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const ShopModifyContainer = styled.div``;
+
+export default ShopModifyContainer;
