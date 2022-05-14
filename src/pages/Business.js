f497a16fcc6416deff64d4f760b91a1b405e432f
jodoll147 <59550913+jodoll147@users.noreply.github.com>
2022-05-14 22:31:11 +0900

    Business 수정

--- src/pages/Business.js
+++ src/pages/Business.js
@@ -2,6 +2,30 @@ import styled from 'styled-components';
 import React, { useState } from 'react';
 import BusinessContainer from './styled/BusinessContainer';
 
+const CafeListContainer = styled.div`
+  position: relative;
+  display: flex;
+  width: 940px;
+  height: 70px;
+  border: 1px solid #f2f2f2;
+  font-family: 'Cafe24';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 70px;
+  .num {
+    position: relative;
+    width: 20px;
+    margin-left: 30px;
+    color: #4ea6a6;
+  }
+  .name {
+    position: relative;
+    margin-left: 10px;
+    color: black;
+  }
+`;
+
 const RegeShop = () => {
   const [files, setFiles] = useState('');
 
@@ -22,6 +46,41 @@ const RegeShop = () => {
         <p>사업자등록증</p>
         <input type={'file'} accept='img/*' onChange={onLoadFile} />
       </div>
+      <div className='infoBox'>
+        <p>사업자 등록은 3~4일 정도 소요됩니다.</p>
+        <p>가게 이미지, 메뉴, 해시태그 등은 사업자등록 후 수정하기를 통해 사용하실 수 있습니다.</p>
+      </div>
+    </div>
+  );
+};
+
+const CafeList = () => {
+  const [cafe, setcafe] = useState([
+    { num: 0, name: '김첨지커피' },
+    { num: 1, name: '명선이커피' },
+  ]);
+  const cafeList = cafe.map(temp => (
+    <CafeListContainer>
+      <div className='num'>{temp.num}</div>
+      <div className='name'>{temp.name}</div>
+    </CafeListContainer>
+  ));
+
+  return cafeList;
+};
+
+const ShopModify = () => {
+  return (
+    <div className='shopModifyBox'>
+      <div className='title'>Business Page</div>
+      <div className='userNameBox'>
+        <div className='userName'>김첨지</div>
+        <div className='info'>사장님 반갑습니다!</div>
+      </div>
+      <div className='shop'>
+        <p>내 가게 관리하기</p>
+        <CafeList />
+      </div>
     </div>
   );
 };
@@ -30,7 +89,7 @@ const Business = () => {
   return (
     <BusinessContainer>
       <section>
-        <RegeShop />
+        <ShopModify />
       </section>
     </BusinessContainer>
   );
--- src/pages/styled/BusinessContainer.js
+++ src/pages/styled/BusinessContainer.js
@@ -6,10 +6,11 @@ const BusinessContainer = styled.div`
     width: 940px;
     height: 680px;
 
-    .regeShopBox {
+    .shopAdd {
       position: relative;
       .title {
         position: relative;
+        margin-top: 40px;
         font-family: 'Cafe24Bold';
         font-style: normal;
         font-size: 18px;
@@ -18,6 +19,7 @@ const BusinessContainer = styled.div`
       }
       .explan {
         position: relative;
+        margin-top: 20px;
         font-family: 'Cafe24';
         font-size: 16px;
         font-weight: 400;
@@ -26,8 +28,13 @@ const BusinessContainer = styled.div`
       .shopBox {
         position: relative;
         display: flex;
+        align-items: baseline;
+        margin-top: 40px;
+
         p {
           position: relative;
+          margin-left: 337px;
+          margin-right: 10px;
           font-family: 'Cafe24';
           font-size: 16px;
           font-weight: 700;
@@ -36,13 +43,23 @@ const BusinessContainer = styled.div`
         }
         input {
           position: relative;
+          width: 200px;
+          height: 25px;
+          background: #d2d9cc;
+          border-radius: 5px;
+          border: #d2d9cc;
         }
       }
+
       .documentBox {
         position: relative;
         display: flex;
+        align-items: baseline;
+
         p {
           position: relative;
+          margin-left: 293px;
+          margin-right: 10px;
           font-family: 'Cafe24';
           font-size: 16px;
           font-weight: 700;
@@ -54,6 +71,77 @@ const BusinessContainer = styled.div`
           font-family: 'Cafe24';
           font-size: 14px;
           line-height: 14px;
+        }
+      }
+      .infoBox {
+        position: relative;
+        margin-top: 50px;
+        margin-left: 293px;
+        p {
+          position: relative;
+          width: 328px;
+          height: 24px;
+          font-family: 'Cafe24';
+          font-size: 12px;
+          line-height: 12px;
+          font-weight: 400;
+          color: #686868;
+          text-align: left;
+        }
+      }
+    }
+    .shopModifyBox {
+      position: relative;
+      text-align: left;
+
+      .title {
+        position: relative;
+        margin-top: 40px;
+        margin-left: 30px;
+        margin-top: 20px;
+        font-family: 'Cafe24Bold';
+        font-style: normal;
+        font-weight: 800;
+        font-size: 18px;
+        line-height: 18px;
+        color: #4ea6a6;
+      }
+      .userNameBox {
+        position: relative;
+        display: flex;
+        margin-left: 30px;
+        margin-top: 20px;
+        .userName {
+          position: relative;
+          font-family: 'Cafe24';
+          font-style: normal;
+          font-weight: 800;
+          font-size: 18px;
+          line-height: 18px;
+        }
+        .info {
+          position: relative;
+          margin-left: 10px;
+          font-family: 'Cafe24';
+          font-style: normal;
+          font-weight: 400;
+          font-size: 16px;
+          line-height: 16px;
+        }
+      }
+      .shop {
+        position: relative;
+        margin-top: 40px;
+        margin-bottom: 10px;
+        font-family: 'Cafe24Bold';
+        font-style: normal;
+        font-weight: 800;
+        font-size: 18px;
+        line-height: 18px;
+        z p {
+          position: relative;
+          margin-top: 40px;
+          margin-left: 30px;
         }
       }
     }
