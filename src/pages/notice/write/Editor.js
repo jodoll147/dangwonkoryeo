332afc08fc8a8b307f879e699751de3bd1dba983
jodoll147 <59550913+jodoll147@users.noreply.github.com>
2022-05-25 23:50:24 +0900

    CoffeeTI state 옮기기

---
+++ src/pages/notice/write/Editor.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.bubble.css';
+import Responsive from './Responsive';
+import styled from 'styled-components';
+
+const EditorBlock = styled(Responsive)`
+  padding-top: 40px;
+  padding-bottom: 0.5rem;S
+  padding-left: 20px;
+  padding-right: 20px;
+  width: 940dpx;
+  height: 1000px;
+  border: 1px solid #d2d9cc;
+  box-sizing: border-box;
+  maxlength: 1000;
+`;
+
+const TitleInput = styled.input`
+  font-size: 30px;
+  outline: none;
+  padding-bottom: 0.5rem;
+  border: none;
+  border-bottom: 3px solid #d2d9cc;
+  margin-bottom: 20px;
+  width: 900px;
+`;
+
+const QuillWrapper = styled.div`
+  .ql-editor {
+    padding: 0;
+    min-height: 320px;
+    font-size: 1.125rem;
+    line-height: 1.5;
+  }
+  .ql-editor .ql-blank::before {
+    left: 0;
+  }
+`;
+
+const Editor = () => {
+  const quillElement = useRef(null);
+  const quillInstance = useRef(null);
+
+  const inputTitle = e => {
+    setTitle(e.target.value);
+    console.log(title);
+  };
+
+  const inputContent = e => {
+    setContent(e.target.value);
+  };
+
+  useEffect(() => {
+    quillInstance.current = new Quill(quillElement.current, {
+      theme: 'bubble',
+      placeholder: '내용을 작성하세요.',
+      modules: {
+        toolbar: [
+          [{ header: '1' }, { header: '2' }],
+          ['bold', 'italic', 'underline', 'strike', 'link'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          ['blockquote', 'code-block', 'link', 'image'],
+        ],
+      },
+    });
+  }, []);
+
+  return (
+    <Responsive>
+      <EditorBlock>
+        <TitleInput placeholder='제목을 입력하세요.' onChange={inputTitle} />
+        <QuillWrapper>
+          <div ref={quillElement} onChange={inputContent} />
+        </QuillWrapper>
+      </EditorBlock>
+    </Responsive>
+  );
+};
+
+export default Editor;
