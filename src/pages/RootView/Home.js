6ae73b813661e42dd5b171d0e654ec24fb599d1d
jodoll147 <59550913+jodoll147@users.noreply.github.com>
2022-05-27 13:25:24 +0900

    home 배너 링크 변경

--- src/pages/RootView/Home.js
+++ src/pages/RootView/Home.js
@@ -11,17 +11,17 @@ import { hover } from '@testing-library/user-event/dist/hover';
 
 const Slide = () => {
   const setting = {
-    dots: true, // 캐러셀이미지가 몇번째인지 알려주는 점을 보여줄지 정한다.
-    infinite: true, // loop를 만들지(마지막 이미지-처음 이미지-중간 이미지들-마지막 이미지)
-    speed: 500, // 애미메이션의 속도, 단위는 milliseconds
-    slidesToShow: 1, // 한번에 몇개의 슬라이드를 보여줄 지
-    slidesToScroll: 1, // 한번 스크롤시 몇장의 슬라이드를 넘길지
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
 
   return (
     <Slider {...setting}>
       <div>
-        <Link to='/'>
+        <Link to='/intro'>
           <img src={dangwonkeoyeo} alt='dangwonkeoyeo' />
         </Link>
       </div>
